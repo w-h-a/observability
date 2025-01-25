1317532e005766653f5edbf921fc0444d7b91b39
Wes Anderson <wesley.herb.anderson@gmail.com>
2025-01-25 14:39:58 -0800

    feat: add basic nav (#41)

---
+++ frontend/src/views/Nav/ShowBreadcrumbs.tsx
@@ -0,0 +1,44 @@
+import { Breadcrumb } from "antd";
+import { Link, withRouter } from "react-router-dom";
+
+const breadcrumbMap: Record<string, string> = {
+	"/application": "Services",
+	"/service-map": "Service Map",
+	"/spans": "Spans",
+};
+
+export const ShowBreadcrumbs = withRouter((props) => {
+	const { location } = props;
+
+	const pathSnippets = location.pathname.split("/").filter((ele) => ele);
+
+	const extraBreadcrumbs = pathSnippets.map((_, idx) => {
+		const url = `/${pathSnippets.slice(0, idx + 1).join("/")}`;
+
+		if (!breadcrumbMap[url]) {
+			return (
+				<Breadcrumb.Item key={url}>
+					<Link to={url}>{url.split("/").slice(-1)[0]}</Link>
+				</Breadcrumb.Item>
+			);
+		} else {
+			return (
+				<Breadcrumb.Item key={url}>
+					<Link to={url}>{breadcrumbMap[url]}</Link>
+				</Breadcrumb.Item>
+			);
+		}
+	});
+
+	const breadcrumbs = [
+		<Breadcrumb.Item key="home">
+			<Link to="/"></Link>
+		</Breadcrumb.Item>,
+	].concat(extraBreadcrumbs);
+
+	return (
+		<div>
+			<Breadcrumb>{breadcrumbs}</Breadcrumb>
+		</div>
+	);
+});
--- frontend/src/views/Nav/TopNav.tsx
+++ frontend/src/views/Nav/TopNav.tsx
@@ -1,11 +1,16 @@
 import { Col, Row } from "antd";
+import { ShowBreadcrumbs } from "./ShowBreadcrumbs";
+import { DateTimeSelector } from "./DateTimeSelector";
 
 export const TopNav = () => {
 	return (
 		<Row>
-			<Col span={16}></Col>
-
-			<Col span={8}></Col>
+			<Col span={16}>
+				<ShowBreadcrumbs />
+			</Col>
+			<Col span={8}>
+				<DateTimeSelector />
+			</Col>
 		</Row>
 	);
 };
