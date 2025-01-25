8bad03079451ad98f66f2d1130778876f8f9a94d
w-h-a <wesley.herb.anderson@gmail.com>
2025-01-25 14:36:31 -0800

    feat: add basic nav

--- frontend/src/views/Nav/SideNav.tsx
+++ frontend/src/views/Nav/SideNav.tsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Menu } from "antd";
+import {
+	AlignLeftOutlined,
+	BarChartOutlined,
+	DeploymentUnitOutlined,
+} from "@ant-design/icons";
 import Sider from "antd/es/layout/Sider";
 
+// TODO: make routes an enum
+
 export const SideNav = () => {
-	return <Sider></Sider>;
+	const [collapsed, setCollapsed] = useState(false);
+
+	const onCollapse = () => {
+		setCollapsed(!collapsed);
+	};
+
+	return (
+		<Sider collapsible collapsed={collapsed} onCollapse={onCollapse} width={160}>
+			<Menu mode="inline">
+				<Menu.Item key="/application" icon={<BarChartOutlined />}>
+					<NavLink
+						to="/application"
+						style={{ fontSize: 12, textDecoration: "none" }}
+					>
+						Services
+					</NavLink>
+				</Menu.Item>
+				<Menu.Item key="/service-map" icon={<DeploymentUnitOutlined />}>
+					<NavLink
+						to="/service-map"
+						style={{ fontSize: 12, textDecoration: "none" }}
+					>
+						Service Map
+					</NavLink>
+				</Menu.Item>
+				<Menu.Item key="/spans" icon={<AlignLeftOutlined />}>
+					<NavLink to="/spans" style={{ fontSize: 12, textDecoration: "none" }}>
+						Spans
+					</NavLink>
+				</Menu.Item>
+			</Menu>
+		</Sider>
+	);
 };
