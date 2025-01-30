58c5a1ff3923473e0b6d8fe351987bd2854090c2
w-h-a <wesley.herb.anderson@gmail.com>
2025-01-30 07:18:03 -0800

    feat: get service map working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,10 @@ import { Layout } from "antd";
 import { SideNav } from "./views/Nav/SideNav";
 import { TopNav } from "./views/Nav/TopNav";
 import { ServicesTable } from "./views/Services/ServicesTable";
+import { ServiceMap } from "./views/Services/ServiceMap";
 import { Service } from "./views/Service/Service";
-import { Spans } from "./views/Spans/Spans";
 import { TraceGraph } from "./views/Traces/TraceGraph";
+import { Spans } from "./views/Spans/Spans";
 import { store } from "./updaters/store";
 import { ClientContext } from "./clients/query/clientCtx";
 
@@ -22,9 +23,10 @@ export const App = () => {
 							<TopNav />
 							<ClientContext.Provider value={useContext(ClientContext)}>
 								<Switch>
-									<Route path="/traces/:id" component={TraceGraph} />
 									<Route path="/spans" component={Spans} />
+									<Route path="/traces/:id" component={TraceGraph} />
 									<Route path="/application/:service" component={Service} />
+									<Route path="/service-map" component={ServiceMap} />
 									<Route path="/application" component={ServicesTable} />
 									<Route
 										path="/"
--- frontend/src/views/Services/ServiceMap.tsx
+++ frontend/src/views/Services/ServiceMap.tsx
@@ -1,3 +1,75 @@
+import { useContext, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ForceGraph2D } from "react-force-graph";
+import { AppDispatch, RootState } from "../../updaters/store";
+import { ServicesUpdater } from "../../updaters/services/services";
+import { ClientContext } from "../../clients/query/clientCtx";
+
 export const ServiceMap = () => {
-    
-}
+	const { queryClient } = useContext(ClientContext);
+
+	const maxMinTime = useSelector((state: RootState) => state.maxMinTime);
+	const services = useSelector((state: RootState) => state.services);
+	const serviceDependencies = useSelector(
+		(state: RootState) => state.serviceDependencies,
+	);
+
+	const dispatch: AppDispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(ServicesUpdater.Services(queryClient, maxMinTime));
+		dispatch(ServicesUpdater.ServiceDependency(queryClient, maxMinTime));
+	}, [dispatch, queryClient, maxMinTime]);
+
+	const graphData = ServicesUpdater.Graph(services, serviceDependencies);
+
+	return (
+		<div>
+			<ForceGraph2D
+				cooldownTicks={100}
+				graphData={graphData}
+				nodeLabel="id"
+				linkAutoColorBy={(d) => d.target}
+				linkDirectionalParticles="value"
+				linkDirectionalParticleSpeed={(d) => d.value}
+				nodeCanvasObject={(node, ctx) => {
+					let color = "#88CEA5";
+					if (Number(node.errorRate) > 0) {
+						color = "#F98989";
+					}
+
+					ctx.fillStyle = color;
+					ctx.beginPath();
+					if (node.x && node.y) {
+						ctx.arc(node.x, node.y, 16, 0, 2 * Math.PI, false);
+					}
+					ctx.fill();
+
+					const fontSize = 6;
+					ctx.font = `${fontSize}px Roboto`;
+
+					const label = node.id;
+
+					ctx.textAlign = "center";
+					ctx.textBaseline = "middle";
+					ctx.fillStyle = "#333333";
+					if (node.x && node.y) {
+						ctx.fillText(label, node.x, node.y);
+					}
+				}}
+				nodePointerAreaPaint={(node, color, ctx) => {
+					ctx.fillStyle = color;
+					ctx.beginPath();
+					if (node.x && node.y) {
+						ctx.arc(node.x, node.y, 5, 0, 2 * Math.PI, false);
+					}
+					ctx.fill();
+				}}
+				onNodeDragEnd={(node) => {
+					node.fx = node.x;
+					node.fy = node.y;
+				}}
+			/>
+		</div>
+	);
+};
